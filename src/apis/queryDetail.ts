5849a41981b389868c2f0f3fcb1181a204494ec1
WiDayn <widayn@prmlk.com>
2022-08-21 23:26:47 +0800

    Update

--- src/apis/queryDetail.ts
+++ src/apis/queryDetail.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 export default {
     queryDetail(clientId:string){
         return axios.get("/queryDetail?ClientId=" + clientId).then((res) => {
-            return res.data.data.client
+            return res.data.data
         })
     }
 }
--- src/components/mainView.vue
+++ src/components/mainView.vue
@@ -12,14 +12,14 @@
     </el-table-column>
     <el-table-column label="服务器名称">
       <template #default="scope">
-          <div @click="detailData = []; nowSelect = scope.$index; loadingDetail = true; getDetail(); drawer = true">
+          <div @click="nowSelect = scope.$index; loadingDetail = true; getDetail(); drawer = true">
             <p >{{ scope.row.DisplayName }}</p>
           </div>
       </template>
     </el-table-column>
     <el-table-column label="所在地">
       <template #default="scope">
-        <el-image style="width: 30px" fit="contain" :src="'https://api.widayn.club/countriesFlags/' + scope.row.CountryCode + '.png'"></el-image>
+        <el-image style="width: 30px" fit="contain" :src="'https://api.widayn.club/countriesFlags/default/' + scope.row.CountryCode + '.png'"></el-image>
       </template>
     </el-table-column>
     <el-table-column prop="MenFree" label="空闲内存" />
@@ -49,10 +49,15 @@
         <el-progress :percentage="scope.row.DiskPercent" />
       </template>
     </el-table-column>
+    <el-table-column label="CT | CU | CM">
+      <template #default="scope">
+        <p>{{ scope.row.CT.AvgRTT }}ms | {{ scope.row.CU.AvgRTT }}ms | {{ scope.row.CM.AvgRTT }}ms</p>
+      </template>
+    </el-table-column>
   </el-table>
 
-  <el-drawer v-model="drawer" title="服务器详情">
-    <el-skeleton style="width: 240px" :loading="loadingDetail" :rows="5" animated>
+  <el-drawer v-model="drawer" title="服务器详情" size="50%">
+    <el-skeleton style="width: 300px" :loading="loadingDetail" :rows="5" animated>
       <template #template>
         <div style="padding: 14px">
           <el-skeleton-item variant="h3" style="width: 50%" />
@@ -65,13 +70,39 @@
         </div>
       </template>
       <template #default>
-        <h3>{{ detailData.DisplayName }}</h3>
-        <el-divider />
-        <p>Hostname: {{ detailData.Hostname }}</p>
-        <p>Location: {{ detailData.Country + " " + detailData.RegionName + " " + detailData.City}}</p>
-        <p>OS: {{ detailData.OS }}</p>
-        <p>ISP: {{ detailData.Isp}}</p>
-        <p>OnlineTime: {{ getDays(detailData.UpdatedAt) }}</p>
+        <div>
+          <h3>{{ detailData.client.DisplayName }}</h3>
+          <el-divider />
+          <p>主机名: {{ detailData.client.Hostname }}</p>
+          <p>位置: {{ detailData.client.Country + " " + detailData.client.RegionName + " " + detailData.client.City}}</p>
+          <p>系统: {{ detailData.client.OS }}</p>
+          <p>运营商: {{ detailData.client.Isp}}</p>
+          <p>在线时长: {{ getDays(detailData.client.UpdatedAt) }}</p>
+        </div>
+        <div>
+          <div id="ChartCPU" :style="{width: '100%', height: '300px'}"></div>
+        </div>
+        <div>
+          <div id="ChartMem" :style="{width: '100%', height: '300px'}"></div>
+        </div>
+        <div>
+          <div id="ChartDisk" :style="{width: '100%', height: '300px'}"></div>
+        </div>
+        <div>
+          <div id="ChartUpStream" :style="{width: '100%', height: '300px'}"></div>
+        </div>
+        <div>
+          <div id="ChartDownStream" :style="{width: '100%', height: '300px'}"></div>
+        </div>
+        <div>
+          <div id="ChartCT" :style="{width: '100%', height: '300px'}"></div>
+        </div>
+        <div>
+          <div id="ChartCU" :style="{width: '100%', height: '300px'}"></div>
+        </div>
+        <div>
+          <div id="ChartCM" :style="{width: '100%', height: '300px'}"></div>
+        </div>
       </template>
     </el-skeleton>
   </el-drawer>
@@ -81,17 +112,18 @@
 import { defineComponent } from 'vue';
 import store from "@/store/store";
 import queryDetail from "@/apis/queryDetail";
-
+import * as echarts from 'echarts'
 
 export default defineComponent({
   data(){
     return{
       tableData: [],
-      detailData: [],
+      detailData: { pingRecords : {CT: {Time: [], AvgRTT: []}, CU:{Time: [], AvgRTT: []}, CM:{Time: [], AvgRTT: []}}, basicRecords: {CPUAvg: [], MemUsedPercent: [], DiskPercent: [], NowUpStreamDataSize: [], NowDownStreamDataSize: [], Time: []} },
       drawer: false,
       nowSelect: 0,
       loadingDetail: true,
       loadingTable: true,
+      loadingCharts: false,
     }
   },
   "mounted"() {
@@ -110,6 +142,8 @@ export default defineComponent({
       queryDetail.queryDetail(this.tableData[this.nowSelect]["ClientId"]).then((res) => {
         this.detailData = res
         this.loadingDetail = false
+      }).then(() => {
+        this.drawLine()
       })
     },
     getDays(startDate:string){
@@ -123,6 +157,377 @@ export default defineComponent({
         return (parseInt(milliseconds) / 1000 / 60 / 60).toFixed(2) + "小时"
       }
       return (parseInt(milliseconds) / 1000 / 60).toFixed(2) + "分钟"
+    },
+    drawLine(){
+      let ChartCPU = echarts.init(document.getElementById('ChartCPU')!)
+      document.getElementById('ChartCPU')!.setAttribute('_echarts_instance_', '')
+      // 绘制图表
+      ChartCPU.setOption({
+        zlevel: 1,
+        z: 1,
+        title: {
+          text: 'CPU占用',
+          left: 'center'
+        },
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'line',
+            animation: true,
+            label: {
+              backgroundColor: '#ccc',
+              borderColor: '#aaa',
+              borderWidth: 1,
+              shadowBlur: 0,
+              shadowOffsetX: 0,
+              shadowOffsetY: 0,
+              color: '#222'
+            }
+          },
+          formatter: '{b} <br> {c}',
+        },
+        xAxis: {
+          type: 'category',
+          data: this.detailData.basicRecords.Time,
+        },
+        yAxis: {
+          type: 'value'
+        },
+        series: [
+          {
+            data: this.detailData.basicRecords.CPUAvg,
+            type: 'line',
+            itemStyle: {
+              color: '#333'
+            },
+            showSymbol: false
+          }
+        ]
+      });
+
+      let ChartMem = echarts.init(document.getElementById('ChartMem')!)
+      document.getElementById('ChartMem')!.setAttribute('_echarts_instance_', '')
+      // 绘制图表
+      ChartMem.setOption({
+        zlevel: 1,
+        z: 1,
+        title: {
+          text: '内存占用',
+          left: 'center'
+        },
+        xAxis: {
+          type: 'category',
+          data: this.detailData.basicRecords.Time,
+        },
+        yAxis: {
+          type: 'value'
+        },
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'line',
+            animation: true,
+            label: {
+              backgroundColor: '#ccc',
+              borderColor: '#aaa',
+              borderWidth: 1,
+              shadowBlur: 0,
+              shadowOffsetX: 0,
+              shadowOffsetY: 0,
+              color: '#222'
+            }
+          },
+          formatter: '{b} <br> {c}',
+        },
+        series: [
+          {
+            data: this.detailData.basicRecords.MemUsedPercent,
+            type: 'line',
+            itemStyle: {
+              color: '#333'
+            },
+            showSymbol: false
+          }
+        ]
+      });
+
+      let ChartDisk = echarts.init(document.getElementById('ChartDisk')!)
+      document.getElementById('ChartDisk')!.setAttribute('_echarts_instance_', '')
+      // 绘制图表
+      ChartDisk.setOption({
+        zlevel: 1,
+        z: 1,
+        title: {
+          text: '硬盘占用',
+          left: 'center'
+        },
+        xAxis: {
+          type: 'category',
+          data: this.detailData.basicRecords.Time,
+        },
+        yAxis: {
+          type: 'value'
+        },
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'line',
+            animation: true,
+            label: {
+              backgroundColor: '#ccc',
+              borderColor: '#aaa',
+              borderWidth: 1,
+              shadowBlur: 0,
+              shadowOffsetX: 0,
+              shadowOffsetY: 0,
+              color: '#222'
+            }
+          },
+          formatter: '{b} <br> {c}',
+        },
+        series: [
+          {
+            data: this.detailData.basicRecords.DiskPercent,
+            type: 'line',
+            itemStyle: {
+              color: '#333'
+            },
+            showSymbol: false
+          }
+        ]
+      });
+
+      // 基于准备好的dom，初始化echarts实例
+      let ChartUpStream = echarts.init(document.getElementById('ChartUpStream')!)
+      document.getElementById('ChartCT')!.setAttribute('_echarts_instance_', '')
+      // 绘制图表
+      ChartUpStream.setOption({
+        zlevel: 1,
+        z: 1,
+        title: {
+          text: '上传流量',
+          left: 'center'
+        },
+        xAxis: {
+          type: 'category',
+          data: this.detailData.basicRecords.Time,
+        },
+        yAxis: {
+          type: 'value'
+        },
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'line',
+            animation: true,
+            label: {
+              backgroundColor: '#ccc',
+              borderColor: '#aaa',
+              borderWidth: 1,
+              shadowBlur: 0,
+              shadowOffsetX: 0,
+              shadowOffsetY: 0,
+              color: '#222'
+            }
+          },
+          formatter: '{b} <br> {c}Kbp/s',
+        },
+        series: [
+          {
+            data: this.detailData.basicRecords.NowUpStreamDataSize,
+            type: 'line',
+            itemStyle: {
+              color: '#333'
+            },
+            showSymbol: false
+          }
+        ]
+      });
+
+      // 基于准备好的dom，初始化echarts实例
+      let ChartDownStream = echarts.init(document.getElementById('ChartDownStream')!)
+      document.getElementById('ChartCT')!.setAttribute('_echarts_instance_', '')
+      // 绘制图表
+      ChartDownStream.setOption({
+        zlevel: 1,
+        z: 1,
+        title: {
+          text: '下载流量',
+          left: 'center'
+        },
+        xAxis: {
+          type: 'category',
+          data: this.detailData.basicRecords.Time,
+        },
+        yAxis: {
+          type: 'value'
+        },
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'line',
+            animation: true,
+            label: {
+              backgroundColor: '#ccc',
+              borderColor: '#aaa',
+              borderWidth: 1,
+              shadowBlur: 0,
+              shadowOffsetX: 0,
+              shadowOffsetY: 0,
+              color: '#222'
+            }
+          },
+          formatter: '{b} <br> {c}Kbp/s',
+        },
+        series: [
+          {
+            data: this.detailData.basicRecords.NowDownStreamDataSize,
+            type: 'line',
+            itemStyle: {
+              color: '#333'
+            },
+            showSymbol: false
+          }
+        ]
+      });
+
+
+      // 基于准备好的dom，初始化echarts实例
+      let ChartCT = echarts.init(document.getElementById('ChartCT')!)
+      document.getElementById('ChartCT')!.setAttribute('_echarts_instance_', '')
+      // 绘制图表
+      ChartCT.setOption({
+        zlevel: 1,
+        z: 1,
+        title: {
+          text: '中国电信',
+          left: 'center'
+        },
+        xAxis: {
+          type: 'category',
+          data: this.detailData.pingRecords.CT.Time,
+        },
+        yAxis: {
+          type: 'value'
+        },
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'line',
+            animation: true,
+            label: {
+              backgroundColor: '#ccc',
+              borderColor: '#aaa',
+              borderWidth: 1,
+              shadowBlur: 0,
+              shadowOffsetX: 0,
+              shadowOffsetY: 0,
+              color: '#222'
+            }
+          },
+          formatter: '{b} <br> {c}',
+        },
+        series: [
+          {
+            data: this.detailData.pingRecords.CT.AvgRTT,
+            type: 'line',
+            itemStyle: {
+              color: '#333'
+            },
+            showSymbol: false
+          }
+        ]
+      });
+      let ChartCU = echarts.init(document.getElementById('ChartCU')!)
+      document.getElementById('ChartCU')!.setAttribute('_echarts_instance_', '')
+      // 绘制图表
+      ChartCU.setOption({
+        zlevel: 1,
+        z: 1,
+        title: {
+          text: '中国联通',
+          left: 'center'
+        },
+        xAxis: {
+          type: 'category',
+          data: this.detailData.pingRecords.CU.Time,
+        },
+        yAxis: {
+          type: 'value'
+        },
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'line',
+            animation: true,
+            label: {
+              backgroundColor: '#ccc',
+              borderColor: '#aaa',
+              borderWidth: 1,
+              shadowBlur: 0,
+              shadowOffsetX: 0,
+              shadowOffsetY: 0,
+              color: '#222'
+            }
+          },
+          formatter: '{b} <br> {c}',
+        },
+        series: [
+          {
+            data: this.detailData.pingRecords.CU.AvgRTT,
+            type: 'line',
+            itemStyle: {
+              color: '#333'
+            },
+            showSymbol: false
+          }
+        ]
+      });
+      let ChartCM = echarts.init(document.getElementById('ChartCM')!)
+      document.getElementById('ChartCM')!.setAttribute('_echarts_instance_', '')
+      // 绘制图表
+      ChartCM.setOption({
+        zlevel: 1,
+        z: 1,
+        title: {
+          text: '中国移动',
+          left: 'center'
+        },
+        xAxis: {
+          type: 'category',
+          data: this.detailData.pingRecords.CM.Time,
+        },
+        yAxis: {
+          type: 'value'
+        },
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'line',
+            animation: true,
+            label: {
+              backgroundColor: '#ccc',
+              borderColor: '#aaa',
+              borderWidth: 1,
+              shadowBlur: 0,
+              shadowOffsetX: 0,
+              shadowOffsetY: 0,
+              color: '#222'
+            }
+          },
+          formatter: '{b} <br> {c}',
+        },
+        series: [
+          {
+            data: this.detailData.pingRecords.CM.AvgRTT,
+            type: 'line',
+            itemStyle: {
+              color: '#333'
+            },
+            showSymbol: false
+          }
+        ]
+      });
     }
   }
 })
